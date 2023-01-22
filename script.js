26af127ec98631a685e4fb7d6a2652451a2f4a47
thuy <109534863+Thuy2y2c@users.noreply.github.com>
2023-01-23 00:09:26 +0700

    mrdamweb

---
+++ script.js
@@ -0,0 +1,11 @@
+function copy(){
+  navigator.clipboard.writeText("https://github.com/Thuy2y2c/mrdambot")
+
+  document.getElementById(`copy`).innerText = `Đã copy!`
+}
+
+function copy2(){
+  navigator.clipboard.writeText("https://discord.gg/EwEAXGmjHp")
+
+  document.getElementById(`copy2`).innerText = `Đã copy discord!`
+}
